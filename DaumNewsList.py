b18a92a243de01d4a9aba6ca8ef0d037e6809d2c
kim1034 <106126405+kim1034@users.noreply.github.com>
2022-10-18 11:41:13 +0900

    Add files via upload

--- DaumNewsList.py
+++ DaumNewsList.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url ='https://news.daum.net/breakingnews/digital'
-result = requests.get(url)
-
+result = requests.get(url, verify=False)
+from collector.CollectorService import get_daum_news
 doc = BeautifulSoup(result.text, 'html.parser')
 
 # <a href='url'> :a태그는 클릭했을 때 해당 url로 이동
@@ -19,4 +19,5 @@ title_list = doc.select('ul.list_news2 a.link_txt')
 # len(list) = 15, index = 0~14
 
 for i, title in enumerate(title_list):
-    print(f'인덱스 번호: {i}, url: {title["href"]}')
+    print(f'인덱스 번호: {i+1}, url: {title["href"]}')
+    get_daum_news(title["href"])  # 뉴스 제목, 본문 수집함수
