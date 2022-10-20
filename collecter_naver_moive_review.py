51aae915c957e6ec80e4909567fe32d77108f0f6
kim1034 <106126405+kim1034@users.noreply.github.com>
2022-10-20 11:31:24 +0900

    영화수집시작

---
+++ collecter_naver_moive_review.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import  BeautifulSoup
+
+################
+# 영화 제목 수집 #
+################
+
+
+# movie_code: 네이버 영화 코드(6자리숫자)
+
+
+# 제목 수집
+def movie_title_crawler(movie_code):
+    pass
+
+
+# 리뷰 수집(리뷰, 평점, 작성자, 작성일자)
+def movie_review_crawler(movie_code):
+    pass
